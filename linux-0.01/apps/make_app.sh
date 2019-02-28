4c377d63ac12d5756bf9bcf65febf5ccaf511ea4
Marko <mveniger@raf.rs>
2019-02-28 18:55:57 +0100

    Added linux-0.01 code. Also added apps directory with make_app.sh scripta and added start.sh script.

---
+++ linux-0.01/apps/make_app.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+if gcc -m32 -o $1.bin $1.c -nostdlib -nostdinc -e main -Ttext=100 -static -fno-builtin ../lib/lib.a -I ../include; then
+cd ..
+mkdir tmp_hd
+sudo mount -o loop,offset=10240 hd_oldlinux.img tmp_hd
+sudo cp apps/$1.bin tmp_hd/root
+sudo umount tmp_hd
+rmdir tmp_hd
+else
+echo 'GCC Compile failed!'
+fi
