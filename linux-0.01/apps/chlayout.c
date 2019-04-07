952750130eac3712413544c568becf0273311aca
Marko <mveniger@raf.rs>
2019-04-07 23:33:18 +0200

    Some modifications to chlayout tool

--- linux-0.01/apps/chlayout.c
+++ linux-0.01/apps/chlayout.c
@@ -1,58 +1,17 @@
 #include <string.h>
 #include <unistd.h>
+#include <fcntl.h>
 
 #define UTIL_IMPLEMENTATION
 #include "utils.h"
 
-
+char filebuff[512];
+char filenamebuff[256];
 
 int main(char *args)
 {
-	int normal_key_map, reverse_key_map;
-	asm(
-		".text;"
-		"jmp cdstart;"
-		".data;"
-		"nmmap:;"
-		".byte 0,27;"
-		".ascii \"1234567890-=\";"
-		".byte 127,9;"
-		".ascii \"qwertyuiop[]\";"
-		".byte 10,0;"
-		".ascii \"asdfghjkl;'\\\\\";"
-		".byte 0, 0;"
-		".ascii \"\\zxcvbnm,./\";"
-		".byte 0,'*,0,32;"
-		".fill 16,1,0;"
-		".byte '-,0,0,0,'+;"
-		".byte 0,0,0,0,0,0,0;"
-		".byte '<;"
-		".fill 10,1,0;"
-		"rvmap:;"
-		".byte 0,27;"
-		".ascii \"0987654321-=\";"
-		".byte 127,9;"
-		".ascii \"qwertyuiop[]\";"
-		".byte 10,0;"
-		".ascii \"asdfghjkl;'\\\\\";"
-		".byte 0, 0;"
-		".ascii \"\\zxcvbnm,./\";"
-		".byte 0,'*,0,32;"
-		".fill 16,1,0;"
-		".byte '-,0,0,0,'+;"
-		".byte 0,0,0,0,0,0,0;"
-		".byte '<;"
-		".fill 10,1,0;"
-		".text;"
-		"cdstart:;"
-		"movl $nmmap, %%eax;"
-		"movl $rvmap, %%ebx;"
-		:"=a" (normal_key_map), "=b" (reverse_key_map)
-	);
 
-	char *layout_names[] = {"NORMAL_US", "REVERSE_NUMBER_US"};
-	char *layouts[] = {(char *)normal_key_map, (char *)reverse_key_map};
-	int argc;
+	int argc, i, fd;
 	char *opt1;
 	char buff[128];
 
@@ -66,20 +25,62 @@ int main(char *args)
 	opt1 = get_argv(args, 1);
 	if(!strcmp(opt1, "--help"))
 	{
-		printstr("Tool chlayout:\n\nThis tool is used to change the layout of the user defined keyboard layout (layouts[2]). \nPlease use one of the currently available options:\n");
-		int i, n = sizeof(layouts) / sizeof(layouts[0]);
-		for(i = 0; i < n; ++i)
-		{
-			itoa(i, buff);
-			printstr(buff);
-			printstr(": ");
-			printstr(layout_names[i]);
-			printstr("\n");
-		}
+		printstr("Tool chlayout:\n\nThis tool is used to change a part of user defined keyboard layout.\nOptions:\n\tKey table: -k file\n\tShift table: -s file\n\tAlt table: -a file\n\nUsage:\nchlayout -opt filename\n\n");
+
 	}
 	else
 	{
-		change_user_layout(layouts[atoi(opt1)], KEY_LAYOUT);
+
+		if(argc < 3)
+		{
+			printerr("No program args, use option --help\n");
+			_exit(1);
+		}
+		int doflag;
+		for(i = 1; i < argc; ++i)
+		{
+			opt1 = get_argv(args, i);
+			doflag = -1;
+			if(!strcmp(opt1, "-k"))
+			{
+				doflag = KEY_LAYOUT;
+			}
+			else if(!strcmp(opt1, "-s"))
+			{
+				doflag = SHIFT_LAYOUT;
+			}
+			else if(!strcmp(opt1, "-a"))
+			{
+				doflag = ALT_LAYOUT;
+			}
+			if(doflag != -1)
+			{
+				if(i + 1 >= argc)
+				{
+					printerr("Argument missing for: ");
+					printerr(opt1);
+					printerr("\n");
+					_exit(1);
+				}
+				strcpy(filenamebuff, get_argv(args, ARG_PWD));
+				if(strlen(filenamebuff) != 1) strcat(filenamebuff, "/");
+				strcat(filenamebuff, get_argv(args, i + 1));
+				fd = open(filenamebuff, O_RDONLY);
+				if(fd < 0)
+				{
+					printerr("Could not open file!\n");
+					printerr("Tried to open: ");
+					printerr(filenamebuff);
+					_exit(1);
+				}
+				read(fd, filebuff, 512);
+				change_user_layout(filebuff, doflag);
+				close(fd);
+				i += 1;
+
+			}
+		}
+		
 	}
 
 	_exit(0);
